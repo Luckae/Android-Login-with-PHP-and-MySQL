b15e958d440252c0e4bb4db45a8ef53c3565e48a
Lawson Luke <35206593+Luckae@users.noreply.github.com>
2019-04-18 06:23:31 -0400

    Update connect.php

--- youcare/connect.php
+++ youcare/connect.php
@@ -4,9 +4,9 @@
 
 //function con(){
 DEFINE("DB_HOST", "localhost");
-DEFINE("DB_USER", "lawsonlu_lawsonlu");
-DEFINE("DB_PASS","youcare12345678");
-DEFINE("DB_NAME","lawsonlu_retro");
+DEFINE("DB_USER", "yourusername");
+DEFINE("DB_PASS","yourpassword");
+DEFINE("DB_NAME","yourdatabase name");
 
 $con = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
